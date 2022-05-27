3d5d929dfeecbd3a840fd43a3872eb5c3513e130
ysdddi <97170421+ysdddi@users.noreply.github.com>
2022-05-27 11:52:38 +0900

    0526

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,9 +1,20 @@
+import Calendar from 'react-calendar';
+import {useState} from 'react';
+import 'react-calendar/dist/Calendar.css';
+import moment from 'moment';
 
 function Main () {
+    const [value, onChange] = useState(new Date());
+    console.log(Calendar);
     return (
         <>
         <div>
-            sjdoooj
+            <Calendar 
+            onChange={onChange}
+            value={value}
+            minDetail="year"
+            />
+            <div>{moment(value).format("YYYY년 MM월 DD일")} </div>
         </div>
         </>
     )
